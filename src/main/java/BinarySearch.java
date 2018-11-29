5d4d8109b02a8d406dc9b5eb00e8ce2430b6837d
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-11-28 19:14:28 -0800

    complete basic binary search

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -1,9 +1,21 @@
 public class BinarySearch {
-    public static void Main (String args) {
+    public static void main (String args) {
+        int[] first = {12, 16, 32, 45, 81, 91, 101, 131};
+        int[] second = {1, 5, 7, 9, 11, 13};
+        int[] third = {};
 
+        System.out.println(BinarySearch(first, 101));
+        System.out.println(BinarySearch(second, 12));
+        System.out.println(BinarySearch(third, 3));
     }
 
     public static int BinarySearch (int[] arr, int value) {
-        return 2;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
