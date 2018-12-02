b257a060bba232f89d34107c80a8c1132e8ef9bd
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-12-02 14:00:00 -0800

    optimize Binary Search Algorithm

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -7,6 +7,7 @@ public class BinarySearch {
         System.out.println(BinarySearch(first, 101));
         System.out.println(BinarySearch(second, 12));
         System.out.println(BinarySearch(third, 3));
+        System.out.println(OptimizedBS(third, 3));
     }
 
     public static int BinarySearch (int[] arr, int value) {
@@ -14,6 +15,24 @@ public class BinarySearch {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == value) {
                 return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int OptimizedBS(int[] arr, int value) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right){
+            int middle = (left + right) / 2;
+            if (arr[middle] < value){
+                
+            } else if (arr[middle] > value){
+
+            } else {
+                return middle;
             }
         }
         return -1;
