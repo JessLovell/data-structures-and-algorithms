95243ecdcbde1d1fba99c00ee6c2c75c52a3dc81
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-12-04 09:02:11 -0800

    to be safe

--- src/test/java/LinkedList/LinkedListTest.java
+++ src/test/java/LinkedList/LinkedListTest.java
@@ -12,6 +12,24 @@ import static org.junit.Assert.*;
 
 public class LinkedListTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
     public void testInsert() {
 
         LinkedList oneList  = new LinkedList();
