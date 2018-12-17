39d3670f258b8a2ebeedc736e7d21ab41a15f001
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-12-17 09:05:14 -0800

    node class created

---
+++ src/main/java/Tree/Node.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class Node<T> {
+    public T data;
+    public Node leftChild;
+    public Node rightChild;
+
+    public Node(T data, Node leftChild, Node rightChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+}
