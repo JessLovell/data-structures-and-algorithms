69de79ba4d4956778b5c35622b8c1cac188ee44b
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-01-16 18:28:35 -0800

    update testing

--- src/main/java/hashTable/leftJoin/LeftJoin.java
+++ src/main/java/hashTable/leftJoin/LeftJoin.java
@@ -5,16 +5,16 @@ import java.util.Set;
 
 public class LeftJoin {
 
-    public static HashMap leftJoin(HashMap left, HashMap right){
+    public static HashMap leftJoin(HashMap<String, String> left, HashMap<String, String> right){
 
         HashMap leftJoin = new HashMap();
         Set<String> keys = left.keySet();
 
         for (String key : keys){
             if (right.containsKey(key)){
-                leftJoin.put(key, new Object[] {left.get(key),right.get(key)});
+                leftJoin.put(key, new String[] {left.get(key),right.get(key)});
             } else {
-                leftJoin.put(key, new Object[] {left.get(key), null});
+                leftJoin.put(key, new String[] {left.get(key), null});
             }
         }
         return leftJoin;
--- src/test/java/hashTable/leftJoin/LeftJoinTest.java
+++ src/test/java/hashTable/leftJoin/LeftJoinTest.java
@@ -11,7 +11,32 @@ public class LeftJoinTest {
     @Test
     public void testLeftJoin(){
 
-        HashMap
+        HashMap one = new HashMap();
+        one.put("Lovell", "Jessica");
+        one.put("Smith", "John");
+        one.put("Johnson", "Tara");
+        one.put("Mohamed", "Zahra");
+
+        HashMap two = new HashMap();
+        two.put("Lovell", "Britney");
+        two.put("Smith", "Jane");
+        two.put("Johnson", "Mike");
+
+        HashMap<String, String[]> testOne = LeftJoin.leftJoin(one, two);
+        assertEquals("Expect size 4", 4, testOne.size());
+        assertTrue("Expect Lovell key", testOne.containsKey("Lovell"));
+        assertTrue("Expect Smith key", testOne.containsKey("Smith"));
+        assertTrue("Expect Johnson key", testOne.containsKey("Johnson"));
+        assertTrue("Expect Mohamed key", testOne.containsKey("Mohamed"));
+//        assertEquals("kjsdhvjk", testOne.get("Lovell").toString());
+
+        HashMap testTwo = LeftJoin.leftJoin(two, one);
+        assertEquals("Expect size 3", 3, testTwo.size());
+        assertTrue("Expect Lovell key", testTwo.containsKey("Lovell"));
+        assertTrue("Expect Smith key", testTwo.containsKey("Smith"));
+        assertTrue("Expect Johnson key", testTwo.containsKey("Johnson"));
+
+
     }
 
 }
