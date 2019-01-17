ed7f139e17095f5cc29c158d921594dbc95fab37
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-01-16 18:41:18 -0800

    testing complete

--- src/test/java/hashTable/leftJoin/LeftJoinTest.java
+++ src/test/java/hashTable/leftJoin/LeftJoinTest.java
@@ -28,15 +28,26 @@ public class LeftJoinTest {
         assertTrue("Expect Smith key", testOne.containsKey("Smith"));
         assertTrue("Expect Johnson key", testOne.containsKey("Johnson"));
         assertTrue("Expect Mohamed key", testOne.containsKey("Mohamed"));
-//        assertEquals("kjsdhvjk", testOne.get("Lovell").toString());
+        assertEquals("Expect Tara", "Tara", testOne.get("Johnson")[0]);
+        assertEquals("Expect Mike", "Mike", testOne.get("Johnson")[1]);
+        assertEquals("Expect Jessica", "Jessica", testOne.get("Lovell")[0]);
+        assertEquals("Expect Britney", "Britney", testOne.get("Lovell")[1]);
+        assertEquals("Expect John", "John", testOne.get("Smith")[0]);
+        assertEquals("Expect Jane", "Jane", testOne.get("Smith")[1]);
+        assertEquals("Expect Zahra", "Zahra", testOne.get("Mohamed")[0]);
+        assertEquals("Expect null", null, testOne.get("Mohamed")[1]);
 
-        HashMap testTwo = LeftJoin.leftJoin(two, one);
+        HashMap<String, String[]> testTwo = LeftJoin.leftJoin(two, one);
         assertEquals("Expect size 3", 3, testTwo.size());
         assertTrue("Expect Lovell key", testTwo.containsKey("Lovell"));
         assertTrue("Expect Smith key", testTwo.containsKey("Smith"));
         assertTrue("Expect Johnson key", testTwo.containsKey("Johnson"));
-
-
+        assertFalse("Expect Mohamed to not be there", testTwo.containsKey("Mohamed"));
+        assertEquals("Expect Britney", "Britney", testTwo.get("Lovell")[0]);
+        assertEquals("Expect Jessica", "Jessica", testTwo.get("Lovell")[1]);
+        assertEquals("Expect Jane", "Jane", testTwo.get("Smith")[0]);
+        assertEquals("Expect John", "John", testTwo.get("Smith")[1]);
+        assertEquals("Expect Mike", "Mike", testTwo.get("Johnson")[0]);
+        assertEquals("Expect Tara", "Tara", testTwo.get("Johnson")[1]);
     }
-
 }
